e8ce198f244defb9ec65ea5bf7b0569010b72d3e
Kyle Regan <kyleregants@gmail.com>
2018-02-09 09:05:24 -0800

    Added mpc

--- c/Build Your Own Lisp/lispy.c
+++ c/Build Your Own Lisp/lispy.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "mpc.h"
 
 //If we are working on Windows we will add these functions
 //instead of using editline
@@ -12,6 +13,7 @@ static char buffer[2048];
 //to that address and return the copy
 char* readline(char* prompt)
 {
+
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
 
@@ -32,6 +34,21 @@ void add_history(char* unused);
 
 int main(int argc, char** argv)
 {
+  //Create some parsers
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expression = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  //Define them with the following Language
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                 \
+             number  : /-?[0-9]+/;                            \
+             operator: '+' | '-' | '*' | '/';                 \
+             expr    : <number> | '(' <operator> <expr>+ ')'; \
+             lispy   : /^/ <operator> <expr>+ /$/;            \
+            ", Number, Operator, Expression, Lispy);
+
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to exit.\n");
 
@@ -42,10 +59,26 @@ int main(int argc, char** argv)
 
     add_history(input);
 
-    printf("%s\n", input);
+    //Attempt to parse the user input
+    mpc_result_t r;
+    if(mpc_parse("<stdin>", input, Lispy, &r))
+    {
+      //On success print the AST
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    }
+    else
+    {
+      //Print the error
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     free(input);
   }
+
+  //Undefine and delete parsers
+  mpc_cleanup(4, Number, Operator, Expression, Lispy);
 
   return 0;
 }
